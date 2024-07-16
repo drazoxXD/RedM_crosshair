e20e3a97eeeb0c1c3860e767937dac68fda7bda6
Drazox <68930610+drazoxXD@users.noreply.github.com>
2024-07-16 19:28:17 +0200

    Update cl_xhair.lua NUi overflow??? IDK
    My friend said you can crash just by sendning NUI almost every second so i madde this shit!
    hope this will fix that!

--- redm_xhair/client/cl_xhair.lua
+++ redm_xhair/client/cl_xhair.lua
@@ -1,29 +1,24 @@
+local debounceTime = 100
+local lastSentTime = 0
+
 CreateThread(function()
     local aiming = false
     while true do
         Wait(500)
         local isAiming = IsPlayerFreeAiming(PlayerId())
-        if isAiming ~= aiming then
+        local currentTime = GetGameTimer()
+
+        if isAiming ~= aiming and currentTime - lastSentTime > debounceTime then
             aiming = isAiming
+            lastSentTime = currentTime
             if aiming then
                 SendNUIMessage("xhairShow")
-                Citizen.InvokeNative(0x4CC5F2FC1332577F ,GetHashKey("HUD_CTX_IN_FAST_TRAVEL_MENU"))
+                Citizen.InvokeNative(0x4CC5F2FC1332577F, GetHashKey("HUD_CTX_IN_FAST_TRAVEL_MENU"))
             else
                 SendNUIMessage("xhairHide")
-                Citizen.InvokeNative(0x8BC7C1F929D07BF3 ,GetHashKey("HUD_CTX_IN_FAST_TRAVEL_MENU")) 
+                Citizen.InvokeNative(0x8BC7C1F929D07BF3, GetHashKey("HUD_CTX_IN_FAST_TRAVEL_MENU"))
             end
         end
+        print(lastSentTime, currentTime)
     end
 end)
-
--- aimsleep = 1000
--- CreateThread(function ()
---     while true do
---         if IsPlayerFreeAiming(PlayerId()) then
---             aimsleep = 1
---         else
---             aimsleep = 1000
---         end
---         Wait(aimsleep)
---     end
--- end)
